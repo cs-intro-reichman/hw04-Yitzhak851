79805d080f91f6d208962d7f53e3381f57e3c701
YBO <yitzhak851@gmail.com>
2024-01-09 16:44:07 +0200

    Update ArrayOps.java
    de-bugging small methods

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,60 +1,73 @@
 public class ArrayOps {
-    static int[] array1 = {1, 2, 1, 1, 2};
-    static int[] array2 = {2, 1, 1};
+    static int[] array1 = { 1, 2 };
+    static int[] array2 = { 1, 2 };
     static int missingInt;
     static int isMissingSum;
     static int isCorrectSum;
     static int firstMaxValue;
     static int secondMaxValue;
-    static boolean isEqualsArrays;
-    static int index;
-// containsSameElements({1, 2, 1, 1, 2}      , {2, 1}           ); // true
-// containsSameElements({2, 2, 3, 7, 8, 3, 2}, {8, 2, 7, 7, 3}  ); // true
-// containsSameElements({1, 2, 3}            , {1, 2, 3}        ); // true
-// containsSameElements({3, -4, 1, 2, 5}     , {1, 3, -4, 5}    ); // false
+    static boolean isEqualsArrays = false;
 
     public static void main(String[] args) {
         System.out.println(findMissingInt(array1));
-        System.out.println(secondMaxValue(array1));
-        System.out.println(containsTheSameElements(array1, array2));
+        // System.out.println(secondMaxValue(array1));
+        containsTheSameElements(array1, array2);
     }
 
     public static int findMissingInt(int[] array1) {
         isMissingSum = 0;
-        for (int i = 0; i < array.length; i++) {
-            isMissingSum += array[i];
+        for (int i = 0; i < array1.length; i++) {
+            isMissingSum += array1[i];
         }
         isCorrectSum = 0;
-        for (int i = 0; i <= array.length; i++) {
+        for (int i = 0; i <= array1.length; i++) {
             isCorrectSum += i;
         }
         missingInt = isCorrectSum - isMissingSum;
         return missingInt;
     }
 
-    public static int secondMaxValue(int[] array) {
-        firstMaxValue = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > firstMaxValue) {
-                firstMaxValue = array[i];
+    public static int secondMaxValue(int[] array1) {
+        firstMaxValue = array1[0];
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] > firstMaxValue) {
+                firstMaxValue = array1[i];
             }
         }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == firstMaxValue) {
-                array[i] = 0;
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] == firstMaxValue) {
+                array1[i] = 0;
             }
         }
-        secondMaxValue = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > secondMaxValue) {
-                secondMaxValue = array[i];
+        secondMaxValue = array1[0];
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] > secondMaxValue) {
+                secondMaxValue = array1[i];
             }
         }
         return secondMaxValue;
     }
 
     public static boolean containsTheSameElements(int[] array1, int[] array2) {
-        
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println("1- " + isEqualsArrays); // 1
+            isEqualsArrays = false;
+
+            for (int j = 0; j < array2.length; j++) {
+                System.out.println("2- " + isEqualsArrays); // 2
+                System.out.println(array1[i] + " " + array2[j]);
+                if (array1[i] == array2[j]) {
+                    isEqualsArrays = true;
+                }
+            }
+            System.out.println("3- " + isEqualsArrays); // 3
+            if (isEqualsArrays == false) {
+                return isEqualsArrays;
+            }
+            System.out.println("4- " + isEqualsArrays); // 4
+        }
+        System.out.println("5- " + isEqualsArrays); // 5
+        return isEqualsArrays;
     }
 
     public static boolean isSorted(int[] array) {
