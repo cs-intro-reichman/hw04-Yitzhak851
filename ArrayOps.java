c56513264dcf2212e86e58e475900e4ace97ccea
YBO <yitzhak851@gmail.com>
2024-01-09 04:57:04 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,24 +1,37 @@
 public class ArrayOps {
+    static int[] array1 = { 0, 1, 2, 3, 4, 6 };
+    static int isMissingSum;
+    static int isCorrectSum;
+    static int missingInt;
+
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        System.out.println(findMissingInt(array1));
     }
 
-    public static int secondMaxValue(int [] array) {
+    public static int findMissingInt(int[] array) {
+        isMissingSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            isMissingSum += array[i];
+        }
+        isCorrectSum = 0;
+        for (int i = 0; i <= array.length; i++) {
+            isCorrectSum += i;
+        }
+        missingInt = isCorrectSum - isMissingSum;
+        return missingInt;
+    }
+
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
         return 0;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         // Write your code here:
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean isSorted(int[] array) {
         // Write your code here:
         return false;
     }
